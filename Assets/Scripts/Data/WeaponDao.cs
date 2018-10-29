e77bc2f4a1ded2bf0bcafe2643936ca551d2c241
Unknown <felix@felixsoum.com>
2018-10-29 02:01:17 -0400

    Add project

---
+++ Assets/Scripts/Data/WeaponDao.cs
@@ -0,0 +1,27 @@
+﻿using Mono.Data.Sqlite;
+using System.Collections.Generic;
+using System.Data;
+
+public static class WeaponDao
+{
+    public static string DatabasePath { get; set; }
+
+    public static List<Weapon> GetAll()
+    {
+        IDbConnection connection = new SqliteConnection(DatabasePath);
+        connection.Open();
+        IDbCommand command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Weapons";
+        IDataReader reader = command.ExecuteReader();
+
+        var weappons = new List<Weapon>();
+        while (reader.Read())
+        {
+            weappons.Add(new Weapon(reader.GetString(0), reader.GetInt32(1)));
+        }
+
+        reader.Close();
+        connection.Close();
+        return weappons;
+    }
+}
---
+++ Assets/Scripts/Presentation/WeaponText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponText : MonoBehaviour
+{
+    private Text text;
+
+    private void Awake()
+    {
+        WeaponDao.DatabasePath = "URI=file:" + Application.dataPath + "/Databases/Weapons.db";
+        text = GetComponent<Text>();
+        foreach (var weapon in WeaponDao.GetAll())
+        {
+            text.text += weapon + "\n";
+        }
+    }
+}
