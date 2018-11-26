088d08bd0f65853d8ad29237bd542edfd6d380d5
Unknown <felix@felixsoum.com>
2018-11-26 01:42:23 -0500

    Add create

--- Assets/Scripts/Data/WeaponDao.cs
+++ Assets/Scripts/Data/WeaponDao.cs
@@ -1,4 +1,5 @@
 ﻿using Mono.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Data;
 
@@ -6,22 +7,36 @@ public static class WeaponDao
 {
     public static string DatabasePath { get; set; }
 
+    public static void Insert(string name, int cost)
+    {
+        ExecuteCommand($"INSERT INTO Weapons VALUES ('{name}', '{cost}')", reader => { });
+    }
+
     public static List<Weapon> GetAll()
+    {
+        var weapons = new List<Weapon>();
+        ExecuteCommand("SELECT * FROM Weapons",
+            reader =>
+            {
+                while (reader.Read())
+                {
+                    weapons.Add(new Weapon(reader.GetString(0), reader.GetInt32(1)));
+                }
+            });
+        return weapons;
+    }
+
+    public static void ExecuteCommand(string commandText, Action<IDataReader> dataHandler)
     {
         IDbConnection connection = new SqliteConnection(DatabasePath);
         connection.Open();
         IDbCommand command = connection.CreateCommand();
-        command.CommandText = "SELECT * FROM Weapons";
+        command.CommandText = commandText;
         IDataReader reader = command.ExecuteReader();
 
-        var weappons = new List<Weapon>();
-        while (reader.Read())
-        {
-            weappons.Add(new Weapon(reader.GetString(0), reader.GetInt32(1)));
-        }
+        dataHandler(reader);
 
         reader.Close();
         connection.Close();
-        return weappons;
     }
 }
---
+++ Assets/Scripts/Presentation/WeaponCreator.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WeaponCreator : MonoBehaviour
+{
+    [SerializeField] InputField nameInput;
+    [SerializeField] InputField costInput;
+
+    private void Awake()
+    {
+        WeaponDao.DatabasePath = "URI=file:" + Application.dataPath + "/Databases/Weapons.db";
+    }
+
+    public void Create()
+    {
+        WeaponDao.Insert(nameInput.text, int.Parse(costInput.text));
+        SceneManager.LoadScene("Read");
+    }
+}
